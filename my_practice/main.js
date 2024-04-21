1a5d57dc983c4b57744c66b889a6192ad614604b
EsraAbdirashid <israApdirashid@gmail.com>
2024-04-21 18:58:14 +0300

    Done

--- my_practice/main.js
+++ my_practice/main.js
@@ -34,4 +34,6 @@ window.onscroll = () =>{
             let header = document.querySelector('header');
             header.classList.toggle('sticky', window.scrollY > 100);
 
+  /*-----------------------remove toggle icon and navbar------------------------------------- */
+  
 }
