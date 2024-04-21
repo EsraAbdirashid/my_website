34f8eac8d65e4dcab44f009a73e5bf9063ccf863
EsraAbdirashid <israApdirashid@gmail.com>
2024-04-21 18:11:14 +0300

    Done

--- my_practice/main.js
+++ my_practice/main.js
@@ -1,6 +1,7 @@
 let menuIcon = document.querySelector('#menu-icon');
-let navbar = document.querySelector(.nav-bar);
+let nav-bar = document.querySelector(.nav-bar);
 
 menuIcon.onclick = ()=>{
         menuIcon.classList.toggle(fa-xmark);
+        nav-bar.classList.toggle()
 }
