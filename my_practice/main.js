3f6d8e9734b52130f35d3e23f575fe6cc093e97f
EsraAbdirashid <israApdirashid@gmail.com>
2024-04-23 18:21:35 +0300

    Done

--- my_practice/main.js
+++ my_practice/main.js
@@ -4,26 +4,25 @@ let menuIcon = document.querySelector('#menu-icon');
 let navbar = document.querySelector('.nav-bar');
 
 menuIcon.onclick = ()=>{
-        menuIcon.classList.toggle(fa-xmark);
-        nav-bar.classList.toggle(active)
+        menuIcon.classList.toggle('fa-xmark');
+        navbar.classList.toggle('active')
 }
 
 /*-----------------------scroll section active link------------------------------------- */
 
 let sections = document.querySelectorAll('section');
-let naclinks = document.querySelectorAll('header nav a');
+let navlinks = document.querySelectorAll('header nav a');
 
 window.onscroll = () =>{
         sections.forEach(sec =>{
-                let top = window.screenY;
+                let top = window.scrollY;
                 let offset = sec.offsetTop - 150;
                 let height = sec.offsetHeight;
                 let id = sec.getAttribute('id');
 
                 if(top >= offset && top < offset + height){
-                        naclinks.forEach.apply(links => {
+                        navlinks.forEach.apply(links => {
                                 links.classList.remove('active');
-
                                 document.querySelector('header nav a[href*=' + id + ']').classList.add('active');
                         });
                 };
